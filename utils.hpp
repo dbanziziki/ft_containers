55184949bf4b91fa88436a17c5124dd94458199c
linzya <alexdavid2@live.fr>
2021-11-25 15:14:11 +0100

    equal

--- utils.hpp
+++ utils.hpp
@@ -35,6 +35,28 @@ template <class InputIterator1, class InputIterator2, class Compare>
 bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
                              InputIterator2 first2, InputIterator2 last2,
                              Compare comp) {}
+
+template <class InputIterator1, class InputIterator2>
+bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2) {
+    while (first1 != last1) {
+        if (!(*first1 == *first2)) return false;
+        ++first1;
+        ++first2;
+    }
+    return true;
+}
+
+template <class InputIterator1, class InputIterator2, class BinaryPredicate>
+bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2,
+           BinaryPredicate pred) {
+    while (first1 != last1) {
+        if (!pred(*first1 == *first2)) return false;
+        ++first1;
+        ++first2;
+    }
+    return true;
+}
+
 }  // namespace ft
 
 #endif  // __UTILS_H__
