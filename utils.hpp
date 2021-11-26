9e32e8f80d334c14ec522108c80c921b8e26cf06
linzya <alexdavid2@live.fr>
2021-11-26 17:23:36 +0100

    type traits

--- utils.hpp
+++ utils.hpp
@@ -17,6 +17,14 @@ typename ft::iterator_traits<InputIterator>::difference_type difference(
     return diff;
 }
 
+struct false_type {
+    static const bool value = false;
+};
+
+struct true_type {
+    static const bool value = true;
+};
+
 template <bool B, class T = void>
 struct enable_if;
 
@@ -24,6 +32,48 @@ template <class T>
 struct enable_if<true, T> {
     typedef T type;
 };
+
+template <class T>
+struct is_integral : public false_type {};
+
+template <>
+struct is_integral<bool> : public true_type {};
+
+template <>
+struct is_integral<char> : public true_type {};
+
+template <>
+struct is_integral<char16_t> : public true_type {};
+
+template <>
+struct is_integral<char32_t> : public true_type {};
+
+template <>
+struct is_integral<wchar_t> : public true_type {};
+
+template <>
+struct is_integral<short> : public true_type {};
+
+template <>
+struct is_integral<int> : public true_type {};
+
+template <>
+struct is_integral<long int> : public true_type {};
+
+template <>
+struct is_integral<long long int> : public true_type {};
+
+template <>
+struct is_integral<unsigned char> : public true_type {};
+
+template <>
+struct is_integral<unsigned short int> : public true_type {};
+
+template <>
+struct is_integral<unsigned long int> : public true_type {};
+
+template <>
+struct is_integral<unsigned long long int> : public true_type {};
 
 }  // namespace ft
 
