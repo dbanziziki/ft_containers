9a07f65127ff89376b70ebb71a9d0bed7acbe503
David Banziziki <dbanzizi@fu-r10-p4.s19.be>
2022-01-17 16:18:06 +0100

    vec test

--- main.cpp
+++ main.cpp
@@ -10,18 +10,16 @@ typedef ft::map<int, std::string>::iterator iterator;
 typedef ft::map<int, std::string>::const_iterator const_iterator;
 
 int main() {
-    ft::map<int, std::string> m;
-
-    for (int i = 0; i < 10; i++) {
-        m.insert(ft::make_pair(i, "Plop"));
+    ft::vector<int> v;
+    for (int i = 0; i < 5; ++i) {
+        v.push_back(i + 42);
     }
-
-    ft::map<int, std::string>::iterator it = m.lower_bound(9);
-    std::cout << it->first << " " << it->second << std::endl;
-    it = m.upper_bound(4);
-    std::cout << it->first << " " << it->second << std::endl;
-    ft::pair<const_iterator, const_iterator> p = m.equal_range(2);
-    std::cout << "Lower bound: " << p.first->first << std::endl;
-    std::cout << "upper bound: " << p.second->first << std::endl;
+    for (ft::vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
+        std::cout << *it << "\n";
+    }
+    std::cout << "Capacity: " << v.capacity() << std::endl;
+    std::cout << "size: " << v.size() << std::endl;
+    v.clear();
+    std::cout << "size: " << v.size() << std::endl;
     return 0;
 }
